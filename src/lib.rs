9aaee2de690e7ad611333efee9a11483651a420d
Christopher Sardegna <github@reagentx.net>
2020-10-03 16:14:53 -0400

    Abstract search algo to separate function, add test for search methods

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use std::process;
 pub struct Config {
     needle: String,
     haystack: String,
+    case_sensitive: bool,
 }
 
 impl Config {
@@ -16,8 +17,9 @@ impl Config {
             let config = Config {
                 needle: args.remove(1),
                 haystack: args.remove(1),
+                case_sensitive: env::var("CASE_INSENSITIVE").is_err(),
             };
-            println!("Searching for {} in {}", config.needle, config.haystack);
+            eprintln!("Searching for {} in {}", config.needle, config.haystack);
             return Ok(config);
         } else if args.len() <= 2 {
             return Err("missing required parameters: needle, haystack");
@@ -31,16 +33,46 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content: String = get_content(&config.haystack);
 
     // Find matching lines
-    print_matches(content, &config.needle);
+    let matched_lines = if config.case_sensitive {
+        search(&content, &config.needle)
+    } else {
+        search_case_insensitive(&content, &config.needle)
+    };
+    print_matches(&matched_lines);
 
     // Successful exit
     Ok(())
 }
 
-fn print_matches(content: String, needle: &String) {
+fn search<'a>(content: &'a str, needle: &str) -> Vec<String> {
+    let mut results = Vec::new();
     for (line_no, line) in content.split("\n").enumerate() {
         if line.contains(needle) {
-            println!("{}: {}", line_no + 1, line);
+            let match_with_line = format!("{}: {}", line_no + 1, line);
+            results.push(match_with_line);
+        }
+    }
+    return results;
+}
+
+fn search_case_insensitive<'a>(content: &'a str, needle: &str) -> Vec<String> {
+    let mut results = Vec::new();
+    let query = needle.to_lowercase();
+    for (line_no, line) in content.split("\n").enumerate() {
+        if line.to_lowercase().contains(&query) {
+            let match_with_line = format!("{}: {}", line_no + 1, line);
+            results.push(match_with_line);
+        }
+    }
+    return results;
+}
+
+fn print_matches(matched_rows: &Vec<String>) {
+    if matched_rows.len() == 0 {
+        eprintln!("Pattern not found!");
+    } else {
+        for line in matched_rows {
+            println!("{}", line);
         }
     }
 }
@@ -48,9 +80,80 @@ fn print_matches(content: String, needle: &String) {
 fn get_content(filename: &str) -> String {
     match fs::read_to_string(filename) {
         Err(err) => {
-            println!("Unable to open file: {}", err);
+            eprintln!("Unable to open file: {}", err);
             process::exit(1)
         }
         Ok(contents) => contents,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive_one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["2: safe, fast, productive."], search(contents, query));
+    }
+
+    #[test]
+    fn case_sensitive_no_result() {
+        let query = "nothere";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(0, search(contents, query).len());
+    }
+
+    #[test]
+    fn case_sensitive_all_result() {
+        let query = "a";
+        let contents = "\
+abicus
+anteater
+atlas";
+
+        assert_eq!(vec!["1: abicus", "2: anteater", "3: atlas"], search(contents, query));
+    }
+
+    #[test]
+    fn case_insensitive_one_result() {
+        let query = "DuCt";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["2: safe, fast, productive."], search_case_insensitive(contents, query));
+    }
+
+    #[test]
+    fn case_insensitive_no_result() {
+        let query = "NO";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(0, search_case_insensitive(contents, query).len());
+    }
+
+    #[test]
+    fn case_insensitive_all_result() {
+        let query = "A";
+        let contents = "\
+abicus
+anteater
+atlas";
+
+        assert_eq!(vec!["1: abicus", "2: anteater", "3: atlas"], search_case_insensitive(contents, query));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use minigrep::Config;
 fn main() {
     // Build config
     let config = Config::new().unwrap_or_else(|err| {
-        println!("Unable to parse arguments: {}", err);
+        eprintln!("Unable to parse arguments: {}", err);
         process::exit(1)
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1)
     }
 }
