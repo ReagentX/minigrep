7669856dd727ebf281501c4f146e885f01113aa4
Christopher Sardegna <gpg@reagentx.net>
2020-10-06 21:00:15 -0400

    Update argument parsing logic

--- src/lib.rs
+++ src/lib.rs
@@ -12,19 +12,26 @@ pub struct Config {
 impl Config {
     pub fn new() -> Result<Config, &'static str> {
         // `args()` returns an Args struct, so transform into an iterator with `collect()`
-        let mut args: Vec<String> = env::args().collect();
-        if args.len() == 3 {
-            let config = Config {
-                needle: args.remove(1),
-                haystack: args.remove(1),
-                case_sensitive: env::var("CASE_INSENSITIVE").is_err(),
-            };
-            eprintln!("Searching for {} in {}", config.needle, config.haystack);
-            return Ok(config);
-        } else if args.len() <= 2 {
-            return Err("missing required parameters: needle, haystack");
-        }
-        return Err("too many parameters passed!");
+        let mut args = env::args();
+        args.next(); // First element is our program name
+
+        let needle = match args.next() {
+            Some(x) => x,
+            None => return Err("missing required parameter `needle`"),
+        };
+        let haystack = match args.next() {
+            Some(x) => x,
+            None => return Err("missing required parameter `haystack`"),
+        };
+
+        let config = Config {
+            needle: needle,
+            haystack: haystack,
+            case_sensitive: env::var("CASE_INSENSITIVE").is_err(),
+        };
+
+        eprintln!("Searching for {} in {}", config.needle, config.haystack);
+        return Ok(config);
     }
 }
 
@@ -121,7 +128,10 @@ abicus
 anteater
 atlas";
 
-        assert_eq!(vec!["1: abicus", "2: anteater", "3: atlas"], search(contents, query));
+        assert_eq!(
+            vec!["1: abicus", "2: anteater", "3: atlas"],
+            search(contents, query)
+        );
     }
 
     #[test]
@@ -132,7 +142,10 @@ Rust:
 safe, fast, productive.
 Pick three.";
 
-        assert_eq!(vec!["2: safe, fast, productive."], search_case_insensitive(contents, query));
+        assert_eq!(
+            vec!["2: safe, fast, productive."],
+            search_case_insensitive(contents, query)
+        );
     }
 
     #[test]
@@ -154,6 +167,9 @@ abicus
 anteater
 atlas";
 
-        assert_eq!(vec!["1: abicus", "2: anteater", "3: atlas"], search_case_insensitive(contents, query));
+        assert_eq!(
+            vec!["1: abicus", "2: anteater", "3: atlas"],
+            search_case_insensitive(contents, query)
+        );
     }
 }
